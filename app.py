7f7d2ff592fd7531556adba6b79be36ed50c4b88
Shashankh Narayan S <103374311+ssn10997@users.noreply.github.com>
2022-04-15 21:05:57 +0530

    add html para tags

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   #return render_template('index.html')
-  return "Hello world!"
+  return "<p>Hello world!</p>"
 
 if __name__ == "__main__":
   app.run(debug=True)
